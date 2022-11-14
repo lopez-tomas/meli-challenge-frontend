3b5b202d49c1031ee732747c8368bb6094363559
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-11-14 20:57:11 -0300

    [FIX]: Nueva estructura para el componente .
    También se importaron los estilos. Utilicé el objeto  para formatear el precio de los items de acuerdo al formato que usamos en Argentina.

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,23 +1,30 @@
 import React from 'react'
 import { Item as ItemInterface } from '@/vite-env.d'
+import './index.sass'
 
 interface Props {
   data: ItemInterface
 }
 
 const Item: React.FC<Props> = ({ data })=> {
+  const nf = new Intl.NumberFormat("es-AR")
+
   return (
-    <li className='Item-container'>
-      <div className='Item'>
-        <a href="#" className='Item-img'>
-          <img className='Item-img' />
+    <article className='Item'>
+      <a href="#" title={data.title} className='Item-img'>
+        <img src={data.picture} />
+      </a>
+      <div className='Item-info'>
+        <a href="#" title={data.title} className='Item-info--title'>
+          {data.title}
         </a>
-        <div className='Item-info'>
-          <a href="#" className='Item-info--title'>{data.title}</a>
-          <a href="#" className='Item-info--price'>$ {data.price.amount}</a>
-        </div>
+        <a href="#" className='Item-info--price'>
+          $ {nf.format(data.price.amount)}
+        </a>
+
+        <p className='Item-info--shipment'>Llega gratis mañana</p>
       </div>
-    </li>
+    </article>
   )
 }
 
