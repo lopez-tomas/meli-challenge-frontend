29735c56d422ae00bf84fac6578a11916a27bf6b
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-11-14 18:21:39 -0300

    [FEAT]: Interfaces creadas.
    Creé las interfaces de  e .

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,25 @@
-/// <reference types="vite/client" />
 declare module '*.png';
 declare module '*.sass' {
   const content: {[className: string]: string};
   export = content;
 }
+
+export interface Item {
+  id: string;
+  title: string;
+  price: {
+    currency: string;
+    amount: number;
+    decimals: number;
+  };
+  picture: string;
+  condition: string;
+  free_shipping: boolean;
+}
+
+export interface ItemDetail extends Item {
+  sold_quantity: number;
+  description: string;
+}
+
+/// <reference types="vite/client" />
