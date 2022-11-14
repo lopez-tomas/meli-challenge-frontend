9c4704c3efaa3c366711a6e5d075c6612f5da6fd
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-11-14 18:27:07 -0300

    [FEAT]: Utilizando nested routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import SearchPage from "./pages/search"
+import SearchPage from "@/pages/search"
+import ResultsPage from "@/pages/results"
+import DetailPage from "@/pages/detail"
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<SearchPage />} />
+          <Route path="/" element={<SearchPage />}>
+            <Route path="/items" element={<ResultsPage />} />
+            <Route path="/items/:id" element={<DetailPage />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
