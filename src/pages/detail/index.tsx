317bf1f128a4f6513b5fd785ead5e51d46516bcc
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-11-15 17:09:57 -0300

    [FEAT]: Cambios menores agregados.

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom'
 import { ItemDetail } from '@/vite-env.d'
 import ItemInfo from '@/components/ItemInfo'
 import ItemPurchase from '@/containers/ItemPurchase'
+import SellerInfo from '@/components/SellerInfo'
 import './styles.sass'
 
 const DetailPage = () => {
@@ -10,20 +11,22 @@ const DetailPage = () => {
   return (
     <main className='DetailPage'>
       <section className='Detail-container'>
-        <section className='Detail-container--images'>
+        <div className='Detail-container--images'>
           <img src={item.picture} alt={item.title} />
-        </section>
+        </div>
 
         <ItemInfo item={item} />
 
         <ItemPurchase item={item} />
       </section>
 
-      <section className='Detail-container Description-container'>
+      <section className='Description-container'>
         <div>
           <h1>Descripción</h1>
           <p>{item.description}</p>
         </div>
+
+        <SellerInfo seller={seller} />
       </section>
     </main>
   )
@@ -43,6 +46,12 @@ const item: ItemDetail = {
   warranty: 'Garantía de fábrica: 12 meses',
   sold_quantity: 1,
   description: 'El iPhone 12 tiene una espectacular pantalla Super Retina XDR de 6.1 pulgadas (1). Un frente de Ceramic Shield, cuatro veces más resistente a las caídas (2). Modo Noche en todas las cámaras, para que puedas tomar fotos increíbles con poca luz. Grabación, edición y reproducción de video en Dolby Vision con calidad cinematográfica. Y el potente chip A14 Bionic. Además, es compatible con los nuevos accesorios MagSafe, que se acoplan fácilmente a tu iPhone y permiten una carga inalámbrica más rápida (3). Que comience la diversión. Avisos Legales (1) La pantalla tiene las esquinas redondeadas. Si se mide en forma de rectángulo, la pantalla tiene 6.06 pulgadas en diagonal. El área real de visualización es menor. (2) La afirmación se basa en la comparación de la parte frontal de Ceramic Shield del iPhone 12 con respecto a la generación anterior de iPhone. (3) Los accesorios se venden por separado. (4) El iPhone 12 es resistente a las salpicaduras, al agua y al polvo, y fue probado en condiciones de laboratorio controladas, con una clasificación IP68 según la norma IEC 60529 (hasta 30 minutos a una profundidad máxima de 6 metros). La resistencia a las salpicaduras, al agua y al polvo no es una condición permanente y podría disminuir como consecuencia del uso normal. No intentes cargar un iPhone mojado; consulta el manual del usuario para ver las instrucciones de limpieza y secado. La garantía no cubre daños producidos por líquidos.'
+}
+
+const seller: any = {
+  location: 'Palermo, Capital Federal',
+  title: 'platinum',
+  level: '5_green',
 }
 
 export default DetailPage
