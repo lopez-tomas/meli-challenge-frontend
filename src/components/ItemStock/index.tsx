7dcae5f912d6e7e8f17fd035b749aa2dfcf4bf98
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-11-15 14:31:57 -0300

    [FEAT]: Componente ItemStock actualizado.
    Modifiqué un poco la estructura y añadí los estilos.

--- src/components/ItemStock/index.tsx
+++ src/components/ItemStock/index.tsx
@@ -1,17 +1,29 @@
 import { FCItemDetail } from '@/vite-env'
+import './styles.sass'
 
 const ItemStock: React.FC<FCItemDetail> = ({ item }) => {
   return (
-    <div className='ItemPurchase--stock'>
-      <h1>Stock disponible</h1>
-      <label htmlFor="quantity">Cantidad:</label>
-      <select name="quantity">
-        <option value="1">1 unidad</option>
-        <option value="2">2 unidades</option>
-        <option value="3">3 unidades</option>
-        <option value="4">4 unidades</option>
-      </select>
-      <p>(4 disponibles)</p>
+    <div className='ItemStock'>
+      <h1 className='ItemStock-title'>Stock disponible</h1>
+      <div>
+        <label
+          className='ItemStock-quantity'
+          htmlFor="quantity"
+        >
+          Cantidad:
+        </label>
+        <select
+          className='ItemStock-select'
+          id="quantity"
+          name="quantity"
+        >
+          <option value="1">1 unidad</option>
+          <option value="2">2 unidades</option>
+          <option value="3">3 unidades</option>
+          <option value="4">4 unidades</option>
+        </select>
+        <p className='ItemStock-msg'>(4 disponibles)</p>
+      </div>
     </div>
   )
 }
