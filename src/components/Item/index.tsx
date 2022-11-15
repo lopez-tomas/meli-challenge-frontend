f474fbb43626ed4ab598a644cb5faa50616a47ba
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-11-14 21:22:08 -0300

    [FEAT]: Navegación al Detalle añadida.
    Utilicé el React Hook  de  junto con atributo  para hacer la navegación desde la lista de resultados al detalle de cada item al hacer click en la imagen, titulo y/o precio del mismo. Además, desactivé el comportamiento por default de las etiquetas .

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,27 +1,47 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Item as ItemInterface } from '@/vite-env.d'
 import './index.sass'
+import React from 'react'
 
 interface Props {
   data: ItemInterface
 }
 
 const Item: React.FC<Props> = ({ data })=> {
+  const navigate = useNavigate()
   const nf = new Intl.NumberFormat("es-AR")
+
+  const goToDetail = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault()
+    navigate(`/items/${data.id}`)
+  }
 
   return (
     <article className='Item'>
-      <a href="#" title={data.title} className='Item-img'>
+      <a
+        href={`/items/${data.id}`}
+        onClick={e => goToDetail(e)}
+        title={data.title}
+        className='Item-img'
+      >
         <img src={data.picture} />
       </a>
       <div className='Item-info'>
-        <a href="#" title={data.title} className='Item-info--title'>
+        <a
+          href={`/items/${data.id}`}
+          onClick={e => goToDetail(e)}
+          title={data.title}
+          className='Item-info--title'
+        >
           {data.title}
         </a>
-        <a href="#" className='Item-info--price'>
+        <a
+          href={`/items/${data.id}`}
+          onClick={e => goToDetail(e)}
+          className='Item-info--price'
+        >
           $ {nf.format(data.price.amount)}
         </a>
-
         <p className='Item-info--shipment'>Llega gratis mañana</p>
       </div>
     </article>
