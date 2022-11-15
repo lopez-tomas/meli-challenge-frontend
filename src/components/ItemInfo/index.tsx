671921f878f9122db446517658b7f086d4a86775
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-11-15 16:03:42 -0300

    [FEAT]: Componente ItemInfo actualizado.
    Se hicieron cambios en la estructura y se agregaron estilos.

--- src/components/ItemInfo/index.tsx
+++ src/components/ItemInfo/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
 import { ItemDetail } from '@/vite-env'
 import { FaRegHeart } from 'react-icons/fa'
+import './styles.sass'
 
 interface Props {
   item: ItemDetail
@@ -11,14 +11,31 @@ const ItemInfo: React.FC<Props> = ({ item }) => {
 
   return (
     <article className='ItemInfo'>
-      <div className='ItemInfo--status'>
-        <p>{item.condition} | {item.sold_quantity} vendidos</p>
+      <div className='ItemInfo-container'>
+        <header className='ItemInfo-status'>
+          <div className='ItemInfo-status--condition'>
+            <p>{item.condition} | {item.sold_quantity} vendidos</p>
+            <FaRegHeart className='Heart-icon' />
+          </div>
+          <div className='ItemInfo-status--title'>
+            <h1>{item.title}</h1>
+          </div>
+        </header>
+
+        <div className='ItemInfo-price'>
+          <h2 className='ItemInfo-price--total'>$ {nf.format(item.price.amount)}</h2>
+          <p className='ItemInfo-price--installments'>en 6x $ {nf.format(item.price.amount / 6)}</p>
+          <a href="#">Ver los medios de pago</a>
+        </div>
       </div>
-      <div className='ItemInfo--price'>
-        <h1 className='TotalPrice'>$ {nf.format(item.price.amount)}</h1>
-        <h2 className='Installments'>En 6x $ {nf.format(item.price.amount / 6)}</h2>
-        <a>Ver los medios de pago</a>
-      </div>
+
+      <style jsx="true">{`
+        .Heart-icon {
+          font-size: 22px;
+          color: #2968c8;
+          cursor: pointer;
+        }
+      `}</style>
     </article>
   )
 }
