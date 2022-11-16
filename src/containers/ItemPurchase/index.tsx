5da66545988e3b3d64c5f895e411c6504b7df8dd
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-11-16 18:22:21 -0300

    [FEAT]: Cambio en la interfaz del componente.
    También se paso la información del vendedor al componente - ItemSeller -

--- src/containers/ItemPurchase/index.tsx
+++ src/containers/ItemPurchase/index.tsx
@@ -1,4 +1,4 @@
-import { FCItemDetail } from '@/vite-env'
+import { ItemDetail, ISeller } from '@/vite-env'
 import ItemShipment from '@/components/ItemShipment'
 import ItemSeller from '@/components/ItemSeller'
 import ItemStock from '@/components/ItemStock'
@@ -7,14 +7,19 @@ import ItemGuarantee from '@/components/ItemGuarantee'
 
 import './styles.sass'
 
-const ItemPurchase: React.FC<FCItemDetail> = ({ item }) => {
+interface Props {
+  item: ItemDetail;
+  seller: ISeller;
+}
+
+const ItemPurchase: React.FC<Props> = ({ item, seller }) => {
   return (
     <article className='ItemPurchase'>
       <div className='ItemPurchase-container'>
         {item?.free_shipping &&
           <ItemShipment />
         }
-        <ItemSeller item={item!} />
+        <ItemSeller item={item!} seller={seller} />
         <ItemStock item={item!} />
         <ItemButtons />
         <ItemGuarantee item={item!} />
