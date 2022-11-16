cd313d0e71b9aa340ae95bb2bb72aeb8bb1ba521
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-11-16 18:13:19 -0300

    [FEAT]: Utilizando componente Link de react-router-dom.
    Ahora la navegación hacia la vista del detalle de cada item se hace a través del componente Link y no a través del hook useNavigate. Esto se debe a que el componente Link permite mandar información en una propiedad - state -. Uso esta propiedad para mandar la información del vendedor.

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,47 +1,40 @@
-import { useNavigate } from 'react-router-dom'
-import { Item as ItemInterface } from '@/vite-env.d'
-import './index.sass'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Item as ItemInterface } from '@/vite-env.d'
+import { formatPrice } from '@/utils/'
+import './index.sass'
 
 interface Props {
   data: ItemInterface
 }
 
 const Item: React.FC<Props> = ({ data })=> {
-  const navigate = useNavigate()
-  const nf = new Intl.NumberFormat("es-AR")
-
-  const goToDetail = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    e.preventDefault()
-    navigate(`/items/${data.id}`)
-  }
-
   return (
     <article className='Item'>
-      <a
-        href={`/items/${data.id}`}
-        onClick={e => goToDetail(e)}
+      <Link
+        to={`/items/${data.id}`}
+        state={{ seller: data.seller }}
         title={data.title}
         className='Item-img'
       >
         <img src={data.picture} />
-      </a>
+      </Link>
       <div className='Item-info'>
-        <a
-          href={`/items/${data.id}`}
-          onClick={e => goToDetail(e)}
+        <Link
+          to={`/items/${data.id}`}
+          state={{ seller: data.seller }}
           title={data.title}
           className='Item-info--title'
         >
           {data.title}
-        </a>
-        <a
-          href={`/items/${data.id}`}
-          onClick={e => goToDetail(e)}
+        </Link>
+        <Link
+          to={`/items/${data.id}`}
+          state={{ seller: data.seller }}
           className='Item-info--price'
         >
-          $ {nf.format(data.price.amount)}
-        </a>
+          $ {formatPrice(data.price.amount)}
+        </Link>
         <p className='Item-info--shipment'>Llega gratis mañana</p>
       </div>
     </article>
