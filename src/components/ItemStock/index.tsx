fedc3272a8d4464b1ac9a2f6fcbff55e4190321d
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-11-16 18:17:09 -0300

    [FEAT]: Creando una cantidad de elementos -option- de acuerdo a la cantidad disponible del item.
    Además, utilicé la funcion itemAvailableQuantity().

--- src/components/ItemStock/index.tsx
+++ src/components/ItemStock/index.tsx
@@ -1,7 +1,18 @@
 import { FCItemDetail } from '@/vite-env'
+import { itemAvailableQuantity } from '@/utils/'
 import './styles.sass'
 
 const ItemStock: React.FC<FCItemDetail> = ({ item }) => {
+  let options = []
+
+  for(let i = 1; i <= item?.available_quantity; i++) {
+    const option = {
+      value: `${i}`,
+      text: `${i} ${i > 1 ? 'unidades' : 'unidad'}`
+    }
+    options.push(option)
+  }
+
   return (
     <div className='ItemStock'>
       <h1 className='ItemStock-title'>Stock disponible</h1>
@@ -17,12 +28,11 @@ const ItemStock: React.FC<FCItemDetail> = ({ item }) => {
           id="quantity"
           name="quantity"
         >
-          <option value="1">1 unidad</option>
-          <option value="2">2 unidades</option>
-          <option value="3">3 unidades</option>
-          <option value="4">4 unidades</option>
+          {options?.map((option, index) => (
+            <option key={index} value={option.value}>{option.text}</option>
+          ))}
         </select>
-        <p className='ItemStock-msg'>(4 disponibles)</p>
+        <p className='ItemStock-msg'>({item?.available_quantity} {itemAvailableQuantity(item?.available_quantity)})</p>
       </div>
     </div>
   )
