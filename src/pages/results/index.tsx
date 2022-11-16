a317c5e03ee225da89701e3a0093c32c08c41ced
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-11-16 13:45:27 -0300

    [FEAT]: Funcionalidad de la página de resultados añadida.
    Todavía necesita unos cambios en el diseño, concretamente con el mensaje de carga, error y la implementación del componente Breadcrumb.

--- src/pages/results/index.tsx
+++ src/pages/results/index.tsx
@@ -1,13 +1,45 @@
+import { useState, useEffect } from 'react'
+import { useSearchParams } from 'react-router-dom'
+
+import useSearchItemsByQuery from '@/hooks/useSearchItemsByQuery'
+import { IAuthor, ICategory, Item as ItemInterface } from '@/vite-env.d'
 import Item from '@/components/Item'
-import { Item as ItemInterface } from '@/vite-env.d'
+
 import { FaInfoCircle } from 'react-icons/fa'
 import './index.sass'
 
+interface ICustomHook {
+  author: IAuthor;
+  categories: ICategory[];
+  items: ItemInterface[];
+  loading: boolean;
+  error: boolean;
+}
+
 const ResultsPage = () => {
+  const [searchParams] = useSearchParams()
+  const search = searchParams.get('search')
+
+  const {
+    author,
+    categories,
+    items,
+    loading,
+    error
+  }: ICustomHook = useSearchItemsByQuery(search)
+
   return (
     <main className='ResultsPage'>
       <section className='Items'>
-        {items.map((item) => (
+        {categories.length > 0 && categories[0]?.name}
+      </section>
+
+      <section className='Items'>
+        {loading && <div>Cargando...</div>}
+
+        {error && <div>Ocurrió un error</div>}
+
+        {!loading && !error && items?.map((item) => (
           <Item key={item.id} data={item} />
         ))}
       </section>
@@ -19,56 +51,5 @@ const ResultsPage = () => {
     </main>
   )
 }
-
-const items: ItemInterface[] = [
-  {
-    id: 'MLA1127704400',
-    title: 'Apple iPhone 12 (64 Gb) - Morado',
-    price: {
-      currency: 'ARS',
-      amount: 260400,
-      decimals: 0,
-    },
-    picture: 'http://http2.mlstatic.com/D_788531-MLA46104947229_052021-I.jpg',
-    condition: 'new',
-    free_shipping: true,
-  },
-  {
-    id: 'MLA930801548',
-    title: 'Apple Iphone 12 (128GB) - Azul',
-    price: {
-      currency: 'ARS',
-      amount: 301199,
-      decimals: 0,
-    },
-    picture: 'http://http2.mlstatic.com/D_839818-MLA45719894955_042021-I.jpg',
-    condition: 'new',
-    free_shipping: true,
-  },
-  {
-    id: 'MLA931457243',
-    title: 'Apple iPhone 11 (128 Gb) - (product)red',
-    price: {
-      currency: 'ARS',
-      amount: 247999,
-      decimals: 0,
-    },
-    picture: 'http://http2.mlstatic.com/D_724631-MLA46114990506_052021-I.jpg',
-    condition: 'new',
-    free_shipping: true,
-  },
-  {
-    id: 'MLA935716081',
-    title: 'Apple iPhone 12 Pro (256 Gb) - Grafito',
-    price: {
-      currency: 'ARS',
-      amount: 439990,
-      decimals: 0,
-    },
-    picture: 'http://http2.mlstatic.com/D_972588-MLA46079293330_052021-I.jpg',
-    condition: 'new',
-    free_shipping: true,
-  }
-]
 
 export default ResultsPage
