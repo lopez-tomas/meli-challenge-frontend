929cf2f68a64ecffb050f79bd3ec7045a4ed3f48
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-11-16 12:55:30 -0300

    [FEAT]: Funcionalidad del form añadida.
    Cuando se busca un producto, mediante el uso del hook useNavigate() se navega a la página de Resultados.

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,11 +1,32 @@
-import { Outlet } from 'react-router-dom'
+import React, { useState, useRef } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 import { FaSearch } from 'react-icons/fa'
+import { clearURL } from '@/utils'
+
 import logo from '@/assets/images/meli-logo.png'
 import './index.sass'
 
 const adImage = "https://http2.mlstatic.com/D_NQ_887100-MLA50801817839_072022-OO.webp"
 
 const SearchPage = () => {
+  const [search, setSearch] = useState('')
+  const navigate = useNavigate()
+  const formRef= useRef(null)
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault()
+    const target = e.target as typeof e.target & {
+      search: { value: string };
+    }
+
+    setSearch(target.search.value)
+
+    navigate({
+      pathname: '/items',
+      search: `search=${clearURL(target.search.value)}`
+    })
+  }
+
   return (
     <>
       <header className='SearchPage'>
@@ -17,11 +38,13 @@ const SearchPage = () => {
             />
           </a>
 
-          <form className='Search-form'>
+          <form ref={formRef} onSubmit={e => handleSubmit(e)} className='Search-form'>
             <input
               className='Search-form--input'
               type="text"
+              name="search"
               placeholder='Buscar productos, marcas y más...'
+              defaultValue={search}
               spellCheck='false'
             />
             <button className='Search-form--button'>
