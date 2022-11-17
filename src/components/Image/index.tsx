1b692d0a2445de069c1f0333f60b302e89dfa365
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-11-17 16:09:28 -0300

    [FEAT]: Nuevo componente - Image - creado.

---
+++ src/components/Image/index.tsx
@@ -0,0 +1,50 @@
+import { useRef } from 'react'
+
+interface ImageProps {
+  picture: string;
+  title: string;
+  active?: boolean;
+  changeSelectedImage:  React.Dispatch<React.SetStateAction<string>>
+  changeSelectedImageTitle:  React.Dispatch<React.SetStateAction<string>>
+}
+
+const Image: React.FC<ImageProps> = ({
+  picture,
+  title,
+  active,
+  changeSelectedImage,
+  changeSelectedImageTitle
+}) => {
+  const imgDiv = useRef<HTMLDivElement>(null)
+
+  const handleHover = () => {
+    if (imgDiv.current != null) {
+      imgDiv.current.style.borderColor = '#3483fa'
+    }
+    changeSelectedImage(picture);
+    changeSelectedImageTitle(title);
+  }
+
+  const handleHoverOut = (e: any) => {
+    if (imgDiv.current != null) {
+      imgDiv.current.style.borderColor = 'rgba(0, 0, 0, .25)'
+    }
+  }
+
+  return (
+    <div
+      ref={imgDiv}
+      className='ItemImages-pictures'
+      >
+      <img
+        onMouseOver={handleHover}
+        onMouseOut={handleHoverOut}
+        className='ItemImages-pictures--pic'
+        src={picture}
+        alt={title}
+      />
+    </div>
+  )
+}
+
+export default Image
