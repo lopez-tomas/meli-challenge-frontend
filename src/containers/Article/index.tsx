336469f497e9f88da9dcfa1933e391ace84ca2d6
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-11-21 19:53:56 -0300

    [FIX]: Borde del Article-container.
    Ahora, para que el Article-container tenga la clase - bordered -, además de recibir el parámetro del mismo nombre, el ancho de la pantalla tiene que ser mayor a 768px; de no ser así, éste no tendra un borde.

--- src/containers/Article/index.tsx
+++ src/containers/Article/index.tsx
@@ -1,3 +1,5 @@
+import { useState, useLayoutEffect } from 'react'
+import { FaWindowClose } from 'react-icons/fa';
 import './styles.sass'
 
 interface Props {
@@ -8,11 +10,24 @@ interface Props {
 }
 
 const Article: React.FC<Props> = ({ name, flex, bordered, children }) => {
+  const [width, setWidth] = useState(0)
+
+  useLayoutEffect(() => {
+    const updateWidth = () => {
+      setWidth(window.innerWidth)
+    }
+
+    window.addEventListener('resize', updateWidth)
+    updateWidth()
+
+    return () => window.removeEventListener('resize', updateWidth)
+  }, [])
+
   return (
     <article className={`Article Article-${name}`}>
       <div
         className={
-          `Article-container ${flex ? 'flex' : ''} ${bordered ? 'bordered' : ''}`}>
+          `Article-container ${flex ? 'flex' : ''} ${bordered && width > 768 ? 'bordered' : ''}`}>
         {children}
       </div>
     </article>
